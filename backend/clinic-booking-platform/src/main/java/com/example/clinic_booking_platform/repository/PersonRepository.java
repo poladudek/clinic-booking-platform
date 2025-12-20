a75eb17256e591950261e921aa8672c9d9940ad0
Marian Wachala <65907084+Maniekw12@users.noreply.github.com>
2025-12-20 18:47:19 +0100

    delete old file structure

--- backend/clinic-booking-platform/src/main/java/com/example/clinic_booking_platform/repository/PersonRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.clinic_booking_platform.repository;
-
-import com.example.clinic_booking_platform.entity.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {
-}
-
